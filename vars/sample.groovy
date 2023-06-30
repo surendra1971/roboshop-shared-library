c234b37c32f4a213db5d71bff0909ef4a01d8a3b
Your Name <you@example.com>
2023-07-01 00:29:45 +0530

    updating the Auto Push

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,7 +1,8 @@
 // declaring a function info 
-def info() {
+def info(message) {
     echo "Hai, I am function whose name is info"
+    echo "I am printing the value of message is ${message}"
 }
 
 // calling the function info 
-info()
+info("Hello")
