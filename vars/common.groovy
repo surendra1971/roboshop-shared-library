00b1cfd028f32d35408674d83db339b8281352c3
Your Name <you@example.com>
2023-07-06 21:22:12 +0530

    updating the Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,7 @@
 def sonarChecks(){
         sh "echo Sonar Checks In Progress"
-        sh "sonar-scanner -Dsonar.host.url=http://172.31.90.35:9000 -Dsonar.projectKey=${COMPONENT} ${ARGS}  -Dsonar.login=admin -Dsonar.password=password"
+        sh "sonar-scanner -Dsonar.host.url=http://172.31.90.35:9000  ${ARGS} -Dsonar.projectKey=${COMPONENT}  -Dsonar.login=${SONARCRED_USR} -Dsonar.password=${SONARCRED_PSW}"
         sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh"
-        sh "sonar-quality-gate.sh ${SONARCRED_USR} ${SONARCRED_PSW} ${SONARURL} ${COMPONENT}"
+        sh "bash -x sonar-quality-gate.sh ${SONARCRED_USR} ${SONARCRED_PSW} ${SONARURL} ${COMPONENT}"       
         sh "echo Sonar Checks Completed"
 }
