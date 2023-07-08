d3a66542b23163618ddc91d5fedef43ac051719c
Your Name <you@example.com>
2023-07-08 13:24:54 +0530

    updating the Auto Push

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,43 +1,57 @@
-def lintChecks(){
-    sh ''' 
-        echo Installing PYLint for ${COMPONENT}
-        #pip3 install pylint
-        #pylint *.py
-        echo lint checks completed for ${COMPONENT}
-    ''' 
+def call(){
+    
+    node(){
+
+        common.lintChecks()
+    }
+    
 }
 
 
-def call(COMPONENT) {
-    pipeline {
-        agent {  label 'WS' }
-        environment {
-            SONARCRED = credentials('SONARCRED') 
-            SONATURL  = "172.31.90.35"
-        }
-        stages {      
-            stage('Lint Checks') {
-                steps { 
-                    script {
-                        lintChecks()
-                    }
-                }
-            }
 
-            stage('Sonar Checks') {
-                steps {                
-                        script {
-                            env.ARGS="-Dsonar.sources=."
-                            common.sonarChecks()
-                        }
-                    }
-                }
 
-            stage('Testing') {
-                steps {                
-                        sh "echo Testing"
-                    }
-                }
-            }                                                                             
-        }
-    }
+
+
+// def lintChecks(){
+//     sh ''' 
+//         echo Installing PYLint for ${COMPONENT}
+//         #pip3 install pylint
+//         #pylint *.py
+//         echo lint checks completed for ${COMPONENT}
+//     ''' 
+// }
+
+
+// def call(COMPONENT) {
+//     pipeline {
+//         agent {  label 'WS' }
+//         environment {
+//             SONARCRED = credentials('SONARCRED') 
+//             SONATURL  = "172.31.90.35"
+//         }
+//         stages {      
+//             stage('Lint Checks') {
+//                 steps { 
+//                     script {
+//                         lintChecks()
+//                     }
+//                 }
+//             }
+
+//             stage('Sonar Checks') {
+//                 steps {                
+//                         script {
+//                             env.ARGS="-Dsonar.sources=."
+//                             common.sonarChecks()
+//                         }
+//                     }
+//                 }
+
+//             stage('Testing') {
+//                 steps {                
+//                         sh "echo Testing"
+//                     }
+//                 }
+//             }                                                                             
+//         }
+//     }
