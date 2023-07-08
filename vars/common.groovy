a1cca02e4c8649685992d8be81f230109a0b02b1
Your Name <you@example.com>
2023-07-08 14:36:27 +0530

    updating the Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -32,11 +32,13 @@ def lintChecks(){
     }
 }
 
-
 def sonarChecks(){
-        sh "echo Sonar Checks In Progress"
-        // sh "sonar-scanner -Dsonar.host.url=http://172.31.90.35:9000  ${ARGS} -Dsonar.projectKey=${COMPONENT}  -Dsonar.login=${SONARCRED_USR} -Dsonar.password=${SONARCRED_PSW}"
-        // sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh"
-        // sh "bash -x sonar-quality-gate.sh ${SONARCRED_USR} ${SONARCRED_PSW} ${SONARURL} ${COMPONENT}"       
-        sh "echo Sonar Checks Completed"
+        stage('Sonar Checks') {
+           sh 'echo Sonar Checks In Progress'
+           // sh 'sonar-scanner -Dsonar.host.url=http://172.31.90.35:9000  ${ARGS} -Dsonar.projectKey=${COMPONENT}  -Dsonar.login=admin -Dsonar.password=password'
+           // sh 'curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh'
+           //sh 'bash -x sonar-quality-gate.sh admin ${SONARCRED_PSW} ${SONARURL} ${COMPONENT}'       
+           sh 'echo Sonar Checks Completed'        
+        }
 }
+
