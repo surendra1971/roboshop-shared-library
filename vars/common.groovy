5bf74bdb51882d78c902fb907fdda5a554507ed5
Your Name <you@example.com>
2023-07-09 23:32:09 +0530

    updating the Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -67,3 +67,52 @@ def testCases() {
         }                        
 }
 
+def artifacts() {
+        
+        stage('Validate Artifact Version') {
+            env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://172.31.92.222:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true" )
+            print UPLOAD_STATUS
+        }                    
+                
+        if(env.UPLOAD_STATUS == "") {
+                stage('Prepare Artifacts') {
+                        if(env.APP_TYPE == "nodejs"){
+                                sh '''
+                                        echo Preparing Artifacts for ${COMPONENT}
+                                        npm install
+                                        zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js                        
+                                '''
+                        }
+                        else if(env.APP_TYPE == "python"){
+                                sh '''
+                                        echo Preparing Artifacts for ${COMPONENT}
+                                        zip -r ${COMPONENT}-${TAG_NAME}.zip *.py *.int  requirements.txt                     
+                                '''
+                        }
+                        else if(env.APP_TYPE == "java"){
+                                sh '''
+                                        echo Preparing Artifacts for ${COMPONENT}
+                                        mvn clean package
+                                        mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar
+                                        zip -r ${COMPONENT}-${TAG_NAME}.zip  ${COMPONENT}.jar          
+                                '''
+                        }
+                        else {
+                                sh '''
+                                        echo Preparing Artifacts for ${COMPONENT}
+                                        cd static
+                                        zip -r ../${COMPONENT}-${TAG_NAME}.zip *                  
+                                '''
+                        }
+                }
+                
+                stage('Upload Artifacts') {
+                        withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
+                                sh "echo Uploading ${COMPONENT} Artifacts To Nexus"
+                                sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://172.31.92.189:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip || true"
+                                sh "echo Uploading ${COMPONENT} Artifacts To Nexus is Completed"                   
+                                
+                        }                
+                }
+        }
+}
